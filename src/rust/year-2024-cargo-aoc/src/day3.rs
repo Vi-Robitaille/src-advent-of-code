7a9985a32b109f4be48b819d9be54ec23f594fe2
Vi Robitaille <vrobitaille@bitfarms.com>
2024-12-15 10:12:41 -0500

    tmp

--- src/rust/year-2024-cargo-aoc/src/day3.rs
+++ src/rust/year-2024-cargo-aoc/src/day3.rs
@@ -1,10 +1,24 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
-type Inp = Vec<Vec<usize>>;
+use nom::{
+    IResult,
+    sequence::delimited,
+    // see the "streaming/complete" paragraph lower for an explanation of these submodules
+    character::complete::char,
+    bytes::complete::{is_not, tag}
+};
+
+type Inp = Vec<String>;
+
+// mul(8,5)
+
+fn parens(input: &str) -> IResult<&str, &str> {
+    delimited(tag("mul("), second, char(')'))
+}
 
 #[aoc_generator(day3)]
 fn parse_input(input: &str) -> Inp {
-    vec![()]
+    input.lines().map(|x| x.to_string()).collect()
 }
 
 #[aoc(day3, part1)]
