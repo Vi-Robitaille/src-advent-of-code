a29c0be62c1a9ce8a86ce504f9c9f0c414ae704c
Vi Robitaille <vrobitaille@bitfarms.com>
2024-12-14 20:26:23 -0500

    adding 2024

---
+++ src/rust/year-2024-cargo-aoc/src/day6.rs
@@ -0,0 +1,18 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+type Inp = Vec<Vec<usize>>;
+
+#[aoc_generator(day6)]
+fn parse_input(input: &str) -> Inp {
+    vec![()]
+}
+
+#[aoc(day6, part1)]
+fn part_one(input: &Inp) -> usize {
+    1
+}
+
+#[aoc(day6, part2)]
+fn part_two(input: &Inp) -> usize {
+    1
+}
