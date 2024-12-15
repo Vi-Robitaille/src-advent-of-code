4552f71692f559b5d4964c88a33fbe24df02cfe5
Vi Robitaille <vrobitaille@bitfarms.com>
2024-12-14 23:45:37 -0500

    git commit -m 'a fucking crime'

--- src/rust/year-2024-cargo-aoc/src/day2.rs
+++ src/rust/year-2024-cargo-aoc/src/day2.rs
@@ -1,5 +1,5 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-
+use itertools::Itertools;
 
 type I = isize;
 type Inp = Vec<Vec<I>>;
@@ -8,9 +8,7 @@ type Inp = Vec<Vec<I>>;
 fn parse_input(input: &str) -> Inp {
     input
         .lines()
-        .map(|x| {
-            x.split(' ').map(|y| y.parse::<I>().unwrap()).collect()
-        })
+        .map(|x| x.split(' ').map(|y| y.parse::<I>().unwrap()).collect())
         .collect()
 }
 
@@ -33,23 +31,16 @@ fn eval_entry(e: &[I]) -> bool {
     let mut state = State::Unset;
     for j in e.windows(2) {
         match (j[0], j[1], state) {
+            (a, b, _) if a.abs_diff(b) > 3 => return false,
             (a, b, _) if a == b => return false,
             // state is Ascending, pair is Descending -> Unsafe
             (a, b, State::Ascending) if (a > b) => return false,
             // state is Descending, pair is Descending -> Safe
-            (a, b, State::Descending) if (a > b) => {
-                if a.abs_diff(b) > 3 {
-                    return false;
-                }
-            }
+            (a, b, State::Descending) if (a > b) => {}
             // state is Unset, pair is Descending -> Set to Descending
             (a, b, State::Unset) if (a > b) => state = State::Descending,
             // state is Ascending, pair is Ascending -> Safe
-            (a, b, State::Ascending) if (a < b) => {
-                if a.abs_diff(b) > 3 {
-                    return false;
-                }
-            }
+            (a, b, State::Ascending) if (a < b) => {}
             // state is Descending, pair is Ascending -> Safe
             (a, b, State::Descending) if (a < b) => return false,
             // state is Unset, pair is Ascending -> Set to Ascending
@@ -62,5 +53,21 @@ fn eval_entry(e: &[I]) -> bool {
 
 #[aoc(day2, part2)]
 fn part_two(input: &Inp) -> usize {
-    1
+    input
+        .iter()
+        .filter_map(|e| eval_entry_part_twooo(&e).then_some(1))
+        .fold(0, |a, b| a + b)
+}
+
+fn eval_entry_part_twooo(e: &[I]) -> bool {
+    (0..e.len())
+        .map(|i| {
+            let mut a = e.to_vec();
+            let _ = a.remove(i);
+            eval_entry(&a)
+        })
+        .counts()
+        .get(&true)
+        .unwrap_or(&0)
+        > &0
 }
